9f7de6d7a38a892363d463672f6cbcbdea70d653
Vinicius Massuchetto <vmassuchetto@gmail.com>
2017-10-19 16:46:00 -0200

    Tratamento de caracteres especiais nas requisições

--- WebserviceCaixa.php
+++ WebserviceCaixa.php
@@ -60,7 +60,7 @@ class WebserviceCaixa {
 			'UNIDADE' => UNIDADE
 		);
 		
-		$this->args = $this->TrimArray(array_merge($padroes,$args));
+		$this->args = $this->CleanArray(array_merge($padroes,$args));
 		
 		// Informações acessíveis aos getters
 		$this->consulta['CEDENTE'] = CEDENTE;
@@ -74,11 +74,28 @@ class WebserviceCaixa {
 	}
 
 	/**
-	 * Aplica `trim` a todos os elementos de um array recursivamente
+	 * Limpa os campos de um array usando `CleanString`
 	 */
-	 function TrimArray($e) {
+	 function CleanArray($e) {
 
-		 return is_array($e) ? array_map(array($this, 'TrimArray'), $e) : trim($e);
+		 return is_array($e) ? array_map(array($this, 'CleanArray'), $e) : $this->CleanString($e);
+	}
+
+	/**
+	 * Formata string de acordo com o requerido pelo webservice
+	 *
+	 * @see https://stackoverflow.com/a/3373364/513401
+	 */
+	function CleanString($str) {
+		$replaces = array(
+			'S'=>'S', 's'=>'s', 'Z'=>'Z', 'z'=>'z', 'À'=>'A', 'Á'=>'A', 'Â'=>'A', 'Ã'=>'A', 'Ä'=>'A', 'Å'=>'A', 'Æ'=>'A', 'Ç'=>'C', 'È'=>'E', 'É'=>'E',
+			'Ê'=>'E', 'Ë'=>'E', 'Ì'=>'I', 'Í'=>'I', 'Î'=>'I', 'Ï'=>'I', 'Ñ'=>'N', 'Ò'=>'O', 'Ó'=>'O', 'Ô'=>'O', 'Õ'=>'O', 'Ö'=>'O', 'Ø'=>'O', 'Ù'=>'U',
+			'Ú'=>'U', 'Û'=>'U', 'Ü'=>'U', 'Ý'=>'Y', 'Þ'=>'B', 'ß'=>'Ss', 'à'=>'a', 'á'=>'a', 'â'=>'a', 'ã'=>'a', 'ä'=>'a', 'å'=>'a', 'æ'=>'a', 'ç'=>'c',
+			'è'=>'e', 'é'=>'e', 'ê'=>'e', 'ë'=>'e', 'ì'=>'i', 'í'=>'i', 'î'=>'i', 'ï'=>'i', 'ð'=>'o', 'ñ'=>'n', 'ò'=>'o', 'ó'=>'o', 'ô'=>'o', 'õ'=>'o',
+			'ö'=>'o', 'ø'=>'o', 'ù'=>'u', 'ú'=>'u', 'û'=>'u', 'ý'=>'y', 'þ'=>'b', 'ÿ'=>'y'
+		);
+		
+		return preg_replace('/[^0-9A-Za-z;,.\- ]/', '', strtoupper(strtr(trim($str), $replaces)));
 	}
 
 	function GetBaseUrl() {
